759117e45400754c0d245a3f76b181783b48015e
zenghui9977 <1049490998@qq.com>
2019-05-11 16:53:00 +0800

    添加缓存名称常量文件，并在其他文件上更换

---
+++ src/utils/constant.js
@@ -0,0 +1,15 @@
+/*
+ * 用户搜索历史字段
+ */
+
+export const USER_SEARCH_HISTORY = "userSearchHistory";
+
+/*
+ * 用户微信信息字段
+ */
+export const USER_WEIXIN_INFO = "userWexinInfo";
+
+/*
+ * 用户注册登录信息字段
+ */
+export const USER_LOGIN_INFO = "userLoginInfo";
