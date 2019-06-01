1a60d69d299b13d8f14718a8b463ab9b07f82757
zenghui9977 <1049490998@qq.com>
2019-06-01 10:55:29 +0800

    注册页面网络通信以及值得传输

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import wepy from 'wepy'
 import 'wepy-async-function'
-import { BASIC_REQUEST_URL } from '../utils/constant'
+import { BASIC_REQUEST_URL , REGISTER_REQUEST_URL, REGISTER_GETVCODE_REQUEST_URL} from '../utils/constant'
 import json2Form from'../utils/utils'
 
 const login = async (email, password) => {
@@ -16,19 +16,18 @@ const login = async (email, password) => {
     return res.data
   }
 }
-
-const register = async (registerInfo) => {
+const register = async (email, vcode, password, name, studentId, major, grade, sex) => {
   let res = await  wepy.request({
-    url: BASIC_REQUEST_URL + '/user/register',
+    url: BASIC_REQUEST_URL + REGISTER_REQUEST_URL,
     data: {
-      email: registerInfo.email,
-      password: registerInfo.password,
-      student_id: registerInfo.student_id,
-      sex: registerInfo.sex,
-      collage: registerInfo.major,
-      grade: registerInfo.grade,
-      edu_bg: registerInfo.name,
-      validate_code: registerInfo.vcode
+      email: email,
+      password: password,
+      student_id: studentId,
+      sex: sex,
+      collage: major,
+      grade: grade,
+      edu_bg: name,
+      validate_code: vcode
     },
     method: 'POST'
   })
@@ -39,13 +38,13 @@ const register = async (registerInfo) => {
 
 const registerVcode = async(email) => {
   let res = await wepy.request({
-    url: 'http://47.107.99.233:5000/user/get_verification_code/', //获取验证码开发者服务器接口地址",
+    url: BASIC_REQUEST_URL + REGISTER_GETVCODE_REQUEST_URL, // 获取验证码开发者服务器接口地址
     data: {
       email: email
     }, //请求的参数",
     method: 'POST',
     header: {
-      'content-type': 'application/json' // 默认值
+      'content-type': 'application/x-www-form-urlencoded' // 默认值
     },
   })
   if (res.statusCode === 200) {
